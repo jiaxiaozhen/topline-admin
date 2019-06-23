e7c72ab332d37543fbbe6c37752b6951773d5c66
jxz <woshijiaxiaozhen@163.com>
2019-06-23 17:55:46 +0800

    完善登录功能

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -8,10 +8,16 @@
         <el-Form-item prop="mobile">
           <el-input v-model="userForm.mobile" placeholder="请输入手机"></el-input>
         </el-form-item>
-        <el-row :gutter="20">
-          <el-col :span="15" prop="code"><el-input v-model="userForm.code" placeholder="请输入验证码" ></el-input></el-col>
-          <el-col :span="8"><el-button type="primary" plain @click="handleCheck">发送验证码</el-button></el-col>
-        </el-row>
+        <el-form-item prop="code">
+          <el-row :gutter="20">
+            <el-col :span="15"><el-input v-model="userForm.code" placeholder="请输入验证码" ></el-input></el-col>
+            <el-col :span="8"><el-button type="primary" plain @click="handleSubmitCheck" :disabled="!!timer">{{ tip }}</el-button></el-col>
+          </el-row>
+        </el-form-item>
+        <el-form-item prop="checked">
+          <el-checkbox v-model="userForm.checked" class="tips"></el-checkbox>
+            <span class="agree-text">我已阅读并同意<a href="#" class="blue">用户协议</a>和<a href="#" class="blue">隐私条款</a></span>
+        </el-form-item>
         <el-row :gutter="20">
           <el-col :span="20"><el-button type="primary" @click="handleLogin" class="loginBtn">登录</el-button></el-col>
         </el-row>
@@ -30,21 +36,41 @@ export default {
     return {
       userForm: {
         mobile: '',
-        code: ''
+        code: '',
+        checked: ''
       },
+      totalTime: 60,
+      tip: '获取验证码',
+      timer: null,
       rules: {
         mobile: [
           { required: true, message: '请输入手机', trigger: 'blur' },
-          { len: 11, message: '请输入11位有效手机号', trigger: 'blur' }
+          { pattern: /^\d{11}$/, message: '请输入11位有效手机号', trigger: 'blur' }
         ],
         code: [
-          { required: true, message: '请输入验证码', trigger: 'blur' }
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { pattern: /^\d{6}$/, message: '请输入6位有效数字', trigger: 'blur' }
+        ],
+        checked: [
+          { required: true, message: '请同意用户协议' },
+          { pattern: /true/, message: '请同意用户协议' }
         ]
       }
     }
   },
   methods: {
-    handleCheck (e) {
+    handleSubmitCheck () {
+      // 提交验证前，验证手机号格式
+      this.$refs['userForm'].validateField('mobile', (errorMessage) => {
+        if (errorMessage) {
+
+        } else {
+          this.handleCheck()
+        }
+      })
+    },
+
+    handleCheck () {
       // 发送验证码
       const mobile = this.userForm.mobile
       axios({
@@ -59,25 +85,12 @@ export default {
           new_captcha: res.data.data.new_captcha,
           product: 'bind'
         },
-        function (captchaObj) {
+        (captchaObj) => {
           // 这里可以调用验证实例 captchaObj 的实例方法
-          captchaObj.onReady(function () {
-            if (e.target.innerText === '发送验证码') {
-              // 停止倒计时才能弹出动态验证
-              // 验证码ready之后才能调用verify方法显示验证码
-              captchaObj.verify()
-            }
-          }).onSuccess(function () {
-            let totalTime = 60
-            let timer = setInterval(function () {
-              if (totalTime > 0) {
-                totalTime--
-                e.target.innerText = totalTime + '秒后重新发送'
-              } else if (totalTime < 1) {
-                clearInterval(timer)
-                e.target.innerText = '发送验证码'
-              }
-            }, 1000)
+          captchaObj.onReady(() => {
+            // 验证码ready之后才能调用verify方法显示验证码
+            captchaObj.verify()
+          }).onSuccess(() => {
             const {
               geetest_challenge: challenge,
               geetest_seccode: seccode,
@@ -93,6 +106,7 @@ export default {
               }
             }).then(res => {
               console.log(res.data)
+              this.timeCount()
             })
           }).onError(function () {
 
@@ -101,6 +115,17 @@ export default {
       })
     },
     handleLogin () {
+      // 登陆前，验证整个表单
+      this.$refs['userForm'].validate((valid) => {
+        if (valid) {
+          this.checkeLogin()
+        } else {
+          return false
+        }
+      })
+    },
+    checkeLogin () {
+      // 登录请求
       axios({
         method: 'POST',
         url: 'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
@@ -115,25 +140,16 @@ export default {
         this.$message.error('登录失败，手机或验证码错误')
       })
     },
-    submitForm (formName) {
-      this.$refs[formName].validate((valid) => {
-        if (valid) {
-          alert('submit!')
-        } else {
-          alert('error submit!!')
-          return false
+    timeCount () {
+      this.timer = setInterval(() => {
+        this.totalTime--
+        this.tip = this.totalTime + '秒后重新发送'
+        if (this.totalTime < 1) {
+          clearInterval(this.timer)
+          this.tip = '获取验证码'
+          this.timer = null
         }
-      })
-    },
-    resetForm (formName) {
-      this.$refs[formName].validate((valid) => {
-        if (valid) {
-          alert('submit!')
-        } else {
-          alert('error submit!!')
-          return false
-        }
-      })
+      }, 1000)
     }
   }
 }
@@ -153,11 +169,22 @@ export default {
     text-align: center;
     .userForm{
       margin-top: 20px;
+      .agree-text {
+        margin-left: -20px;
+        color:#999;
+        .blue {
+        color: #3296fa;
+        text-decoration:none;
+        }
+      }
       .loginBtn{
       width: 123%;
-      margin-top: 20px
     }
     }
   }
 }
+.tips{
+  margin-left: -80px;
+}
+
 </style>
